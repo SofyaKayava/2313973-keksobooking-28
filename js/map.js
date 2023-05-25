a7685a572872156a4bc2aeaf50b154d8dbe9af83
SofyaKayava <kayava30@gmail.com>
2023-05-25 22:23:34 +0200

    Все

---
+++ js/map.js
@@ -0,0 +1,79 @@
+import {activiteWebPage} from './activate.js';
+import {getAdPopup} from './thumbnails.js';
+
+const TILE_LAYER = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const COPYRIGHT = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const ZOOM = 10;
+const cityCenter = {
+  lat: 35.68948,
+  lng: 139.69170,
+};
+const map = L.map('map-canvas').on('load', () => {
+  activiteWebPage();
+}).setView(cityCenter, ZOOM);
+
+L.tileLayer(TILE_LAYER, {
+  attribution: COPYRIGHT
+}).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: '/img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26,52],
+});
+
+const marker = L.marker(cityCenter, {
+  draggable: true,
+  icon: mainPinIcon,
+  autoPan: true,
+  autoPanPadding: L.point([100,100])
+});
+marker.addTo(map);
+
+const pinIcon = L.icon ({
+  iconUrl: '/img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20,40],
+  popupAnchor: [0, -20]
+});
+
+let ads = [];
+
+function renderAdsPins (items) {
+  items.map((item) =>
+    L.marker(item.location,{icon: pinIcon})
+      .bindPopup(getAdPopup(item))
+      .addTo(map)
+  );
+}
+
+function initAds (newAds) {
+  ads = newAds;
+  renderAdsPins(ads.slice(0, 10));
+}
+
+function closePopup () {
+  map.closePopup();
+}
+
+const fieldAddress = document.querySelector('#address');
+
+function showLatLnginFieldAddress (evt) {
+  const newValue = evt.target.getLatLng();
+  const fixedLat = newValue.lat.toFixed(5);
+  const fixedLng = newValue.lng.toFixed(5);
+  fieldAddress.value = `${fixedLat}, ${fixedLng}`;
+}
+
+marker.on('drag', showLatLnginFieldAddress);
+
+function resetMarker () {
+  marker.setLatLng(cityCenter);
+}
+
+function resetFieldAddress () {
+  fieldAddress.value = `${cityCenter.lat}, ${cityCenter.lng}`;
+}
+
+export {closePopup,resetMarker,resetFieldAddress,initAds};
+
